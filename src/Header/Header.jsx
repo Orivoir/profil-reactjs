1b07e1b9502fbee1e2bc62486424ed4264d5918b
S.Gaborieau <sam.gabor@hotmail.com>
2019-08-09 03:59:17 +0200

    navbar title (origin header)

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,17 +4,24 @@ import {Theme} from './../Theme' ;
 import {Title} from './Title';
 import {Navbar} from './Navbar';
 
+import logo from './logo.png' ;
+import './Header.css' ;
+
 export default class Header extends React.Component {
     
     static contextType = Theme ;
 
     render() {
 
-        return(
+        return (
             
             <header className={ this.context } >
 
-                <Title />
+                <Title
+                    text="Samuel Gaborieau"
+                    bq="profil &amp; parcours du web"
+                    logo={logo}
+                />
                 <Navbar /> 
 
             </header>
--- src/Header/Title.js
+++ src/Header/Title.js
@@ -6,7 +6,16 @@ export const Title = ({text , bq , logo}) => (
     <section id="wrap-title">
 
         <Link to="/">
-            <img src={ logo } alt="logo home" />
+            
+            <img
+                src={ logo }
+                alt="logo home"
+
+                // Default dim (px)
+                width="75"
+                height="75"
+            />
+            
         </Link>
 
         <h1> { text || 'Lorem Ipsum' } </h1>
