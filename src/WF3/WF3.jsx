77f9b9ae15fbbc6f0f97da0933f3fe4cf88001dd
S.Gaborieau <sam.gabor@hotmail.com>
2019-08-14 09:29:45 +0200

    finish integrate wf3 component and theme

--- src/WF3/WF3.jsx
+++ src/WF3/WF3.jsx
@@ -31,7 +31,16 @@ export default class WF3 extends Code {
                 </div>
 
                 <p>
-                    j'intégre WebForce 3 en juillet 2018 ( Bourgogne Franche-Compté )<br />
+                    j'intégre&nbsp;
+                    <a
+                        href="https://wf3.fr"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        WebForce 3
+                    </a>
+                    &nbsp;
+                    en juillet 2018 ( Bourgogne Franche-Compté )<br />
                     pour suivre une formation <b>intensive</b> de 3 mois &frac12;<br />
                     où j'apprend en accéléré de nombreuse technologie du développement web<br />
                     autant les bases fondamentale ( HTML5 &amp; CSS3 ) ainsi que ses bonne pratique
@@ -43,15 +52,54 @@ export default class WF3 extends Code {
                     <Link to="/">
                        développeur "nomade"
                     </Link><br />
-                    grace au documentation du web notament grâce à Mozilla Developpement Network,&nbsp;aux
-                    tutoriel Openclassrooms , Khan Academy , Solo learn , ...&nbsp;
+                    grace au documentation du web notament grâce à&nbsp;
+                    <a
+                        href="https://developer.mozilla.org/fr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        Mozilla Developpement Network
+                    </a>
+                    ,&nbsp;aux
+                    tutoriel&nbsp;
+                    <a 
+                        href="https://openclassrooms.com/fr/membres/samuelgaborieau3"
+                        target="blank"
+                        rel="noopener noreferrer"
+                    >
+                        Openclassrooms
+                    </a>
+                    &nbsp;,&nbsp;
+                    <a
+                        href="https://fr.khanacademy.org/profile/Orivoir21/"
+                        target="blank"
+                        rel="noopener noreferrer"
+                    >
+                        Khan Academy
+                    </a>&nbsp;,&nbsp;
+                    <a
+                        href="https://www.codecademy.com/profiles/Orivoir21"
+                        target="blank"
+                        rel="noopener noreferrer"
+                    >
+                        CodeAcademy
+                    </a>
+                    &nbsp;,&nbsp;
+                    <a
+                        href="https://www.sololearn.com/Profile/6644849"
+                        target="blank"
+                        rel="noopener noreferrer"
+                    >
+                        Solo learn
+                    </a>
+                    &nbsp;, ...&nbsp;
                     et autres <br />
                     aujourd'hui désireux d'intégré une équipe de développement et/où  de contribué à des projets en situation réelle<br />
                     je deploie cette application de présentation de mon parcours .
                 </p>
 
                 <div id="certif">
-                    <div id="certif-integ">
+                    <div className="wrap-pdf" id="certif-integ">
                         <h3>Certification Développement</h3>
                         <Iframe
                             src={ dev }
@@ -59,10 +107,12 @@ export default class WF3 extends Code {
                     </div>
 
                     <div id="certif-dev">
-                        <h3>Certification Intégration</h3>
-                        <Iframe
-                            src={ integ }
-                        />
+                        <div className="wrap-pdf">
+                            <h3>Certification Intégration</h3>
+                            <Iframe
+                                src={ integ }
+                                />
+                        </div>
                     </div>
                 
                 </div>
